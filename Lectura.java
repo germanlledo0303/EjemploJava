5270e58a7759c0e1a8c16e20e98e60b7f58db6cb
Germán <lledo.pena.german@iescamas.es>
2025-09-18 19:25:08 +0200

    Add files via upload

---
+++ Lectura.java
@@ -0,0 +1,18 @@
+package com;
+
+import java.util.*;
+
+public class Lectura {
+
+	public static void main(String[] args) {
+
+		var sc = new Scanner(System.in);
+		
+		System.out.print("Introduce tu nombre: ");
+		String nombre = sc.nextLine();
+		
+		System.out.print("HOLA: " + nombre);
+	
+	}
+
+}
---
+++ TipoDato.java
@@ -0,0 +1,31 @@
+package com;
+
+public class TipoDato {
+
+	public static void main(String[] args) {
+		
+		//Comentarios
+		
+		/*jslajfdl
+		 * safsa
+		 * afa
+		 * ada
+		 * dd
+		 * dddafa
+		 */
+		
+		
+		int numero = 100;
+		float numero_decimal = 1.1F;
+		double numero_decimal2 = 1.5;
+		boolean logico = true;
+		String palabra = "Tomate";
+		char letra = '3';
+		
+		System.out.println("numero " + 100);
+		System.out.println("decimal " + numero_decimal);
+		System.out.println("logico " + logico);	
+		
+	}
+
+}
